0b5be098bb5dc5370d227f7181995230ae168416
Ed Bergavera <edbergavera@gmail.com>
2019-08-03 09:48:05 +0800

    Add more validation

--- ipinfo.py
+++ ipinfo.py
@@ -5,6 +5,7 @@ import socket
 import requests
 import sys
 from tldextract import extract
+import validators
 
 DOMAIN_REGX = r'([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}'
 
@@ -24,12 +25,20 @@ def request_to_ipinfo(ip):
 def main(argv):
     ip = socket.gethostbyname(argv.lower())
     ip_info = request_to_ipinfo(ip)
+    
+    # match = re.search(DOMAIN_REGX, argv.lower())
+    # ip_info = []
+    # if match:
+    #     ip = socket.gethostbyname(argv)
+    #     ip_info = request_to_ipinfo(ip)
+    # else:
+    #     ip_info = request_to_ipinfo(argv)
 
     all_keys = [
         'ip', 'hostname', 'city', 'region', 'country',
         'org', 'postal', 'loc'
     ]
-
+    print('host: ' + argv.lower())
     for k in all_keys:
         try:
             print(u'{}'.format(ip_info[k]))
@@ -38,14 +47,24 @@ def main(argv):
 
 
 if __name__ == "__main__":
+    
+    ARG1 = sys.argv[1]
+    
     if len(sys.argv) == 1:
         print(request_to_ipinfo("")["ip"])
         sys.exit(0)
-    tsd, td, ts = extract(sys.argv[1])
-    dom = td + '.' + ts
+
+    if validators.ipv4(ARG1):
+        main(ARG1)
+        sys.exit(0)
+
+    if validators.url(ARG1) or validators.domain(ARG1):
+        tsd, td, ts = extract(ARG1)
+        host = td + '.' + ts
+    host = ARG1
     try:
-        socket.gethostbyname(dom)
+        socket.gethostbyname(host)
     except socket.gaierror:
         print("Host is invalid or not resolvable.")
         sys.exit(1)
-    main(dom)
+    main(host)
